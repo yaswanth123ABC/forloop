8285bccd08648b10b99ce89bb899341f5bf023fa
yaswanth <yaswanth@turingsystems.com>
2022-02-09 13:10:56 +0530

    Add project files.

---
+++ forloop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Conditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.WriteLine("i value: {0}", i);
+            }
+            Console.WriteLine("Print the values");
+            Console.ReadLine();
+        }
+    }
+}
